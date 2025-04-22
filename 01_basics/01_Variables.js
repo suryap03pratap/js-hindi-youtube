1086db99ed94c9bb50119e2d098e7adca71a7fde
suryap03pratap <125127841+suryap03pratap@users.noreply.github.com>
2025-04-22 20:55:29 +0000

    Var and datatypes

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountid = 144446
+let accountEmail = "hitesh@google.com"
+var accountpassword = "12345"
+accountCity = "Jaipur"
+let accountstate;
+
+accountEmail = "hc@hc.com"
+accountpassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountid)
+console.table([accountid, accountEmail, accountpassword, accountCity,accountstate ])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat alll js code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+console.log(3+3)
+
+console.log("Hitesh")
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number=> 2 to power 53
+// bigint 
+// string => true/false
+//null=> standalone value
+//undefined => 
+// //symbol=> unique
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
